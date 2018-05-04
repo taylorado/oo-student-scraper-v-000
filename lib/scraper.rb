446b93d07d0fcfdfcf3f61c1d0ba5b7e4133b17a
Taylor Donlan <taylordonlan@gmail.com>
2018-05-04 20:24:21 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,23 +4,26 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    students_index = Nokogiri::HTML(open(index_url))
     scraped_students = []
-    students_index = Nokogiri::HTML(open(index_url))  
-
     #iterate over each student-card, adding a hash (of the student's name, location and profile) to the scraped_students array each time
     students_index.css("div.student-card").each do |student|
       scraped_students << {
         :name => student.css("h4.student-name").text,
         :location => student.css("p.student-location").text,
         :profile_url => student.css("a").attribute("href").value
-        }
+          }
     end
         scraped_students
   end
 
   def self.scrape_profile_page(profile_url)
-    student_profile = {}
     profile_page = Nokogiri::HTML(open(profile_url))
+    student_profile = {
+      :profile_quote => profile_page.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text,
+      :bio => profile_page.css("div.main-wrapper.profile .description-holder p").text
+        }
+
 
     # iterate over each social icon container and add each link to student profile hash (only if present)
     profile_page.css("div.main-wrapper.profile .social-icon-container a").each do |social_link|
@@ -35,11 +38,8 @@ class Scraper
       end
     end
 
-    student_profile[:profile_quote] = profile_page.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
-    student_profile[:bio] = profile_page.css("div.main-wrapper.profile .description-holder p").text
-
     student_profile
   end
 
-  
+
 end
