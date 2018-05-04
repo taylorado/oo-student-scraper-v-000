221fb4befcf5d95cc22f7e8e039e9738bec1aced
Taylor Donlan <taylordonlan@gmail.com>
2018-05-04 19:06:41 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,8 +6,19 @@ class Scraper
   def self.scrape_index_page(index_url)
 
 
+    get_page = Nokogiri::HTML(open(index_url))
+
     scraped_students = []
+    get_page.css("div.student-card").each do |student|
+      scraped_students << {
+        :name => student.css("h4").text,
+        :location => student.css("p").text,
+        :profile_url => index_url + student.at("a").attributes["href"].value
+      }
+    end
+      scraped_students
   end
+  
 
   def self.scrape_profile_page(profile_url)
 
