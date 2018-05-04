68ad09da037ca06b2df37ebff2927dccf2e3ad14
Taylor Donlan <taylordonlan@gmail.com>
2018-05-04 19:26:47 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,42 +4,42 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-
-
-    get_page = Nokogiri::HTML(open(index_url))
-
-    scraped_students = []
-    get_page.css("div.student-card").each do |student|
-      scraped_students << {
-        :name => student.css("h4").text,
-        :location => student.css("p").text,
-        :profile_url => index_url + student.at("a").attributes["href"].value
-      }
+    students = []
+    html = open(index_url)
+    index = Nokogiri::HTML(html)
+    index.css("div.student-card").each do |student|
+      student_details = {}
+      student_details[:name] = student.css("h4.student-name").text
+      student_details[:location] = student.css("p.student-location").text
+      profile_path = student.css("a").attribute("href").value
+      student_details[:profile_url] = './fixtures/student-site/' + profile_path
+      students << student_details
     end
-      scraped_students
+    students
   end
 
+ def self.scrape_profile_page(profile_url)
+    student_profile = {}
+    html = open(profile_url)
+    profile = Nokogiri::HTML(html)
 
-    def self.scrape_profile_page(profile_url)
-      get_page = Nokogiri::HTML(open(profile_url))
+    # Social Links
 
-      student_profile = {
-        :profile_quote => get_page.css(".profile-quote").text,
-        :bio => get_page.css("p").text
-      }
-
-      get_page.css("div.social-icon-container a").each do |social|
-        case social.at("img").attributes["src"].value
-        when "../assets/img/twitter-icon.png"
-          student_profile[:twitter] = social.attributes["href"].value
-        when "../assets/img/linkedin-icon.png"
-          student_profile[:linkedin] = social.attributes["href"].value
-        when "../assets/img/github-icon.png"
-          student_profile[:github] = social.attributes["href"].value
-        when "../assets/img/rss-icon.png"
-          student_profile[:blog] = social.attributes["href"].value
-        end
+    profile.css("div.main-wrapper.profile .social-icon-container a").each do |social|
+      if social.attribute("href").value.include?("twitter")
+        student_profile[:twitter] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("linkedin")
+        student_profile[:linkedin] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("github")
+        student_profile[:github] = social.attribute("href").value
+      else
+        student_profile[:blog] = social.attribute("href").value
       end
-      student_profile
     end
+
+    student_profile[:profile_quote] = profile.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
+    student_profile[:bio] = profile.css("div.main-wrapper.profile .description-holder p").text
+
+    student_profile
   end
+end
