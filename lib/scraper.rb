ee2613ebf1c38b494abdbd67d81929ab993c4e3b
Taylor Donlan <taylordonlan@gmail.com>
2018-05-04 19:07:27 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,9 +19,27 @@ class Scraper
       scraped_students
   end
 
-
-  def self.scrape_profile_page(profile_url)
-
+  
+    def self.scrape_profile_page(profile_url)
+      get_page = Nokogiri::HTML(open(profile_url))
+  
+      student_profile = {
+        :profile_quote => get_page.css(".profile-quote").text,
+        :bio => get_page.css("p").text
+      }
+  
+      get_page.css("div.social-icon-container a").each do |social|
+        case social.at("img").attributes["src"].value
+        when "../assets/img/twitter-icon.png"
+          student_profile[:twitter] = social.attributes["href"].value
+        when "../assets/img/linkedin-icon.png"
+          student_profile[:linkedin] = social.attributes["href"].value
+        when "../assets/img/github-icon.png"
+          student_profile[:github] = social.attributes["href"].value
+        when "../assets/img/rss-icon.png"
+          student_profile[:blog] = social.attributes["href"].value
+        end
+      end
+      student_profile
+    end
   end
-
-end
